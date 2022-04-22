44b00f38a7f66f58e7479f9f8a937df2b39b746d
PathonScript <pathonscript@gmail.com>
2022-04-22 17:03:26 +0700

    fixed stopping

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ dotenv.config({
 })
 
 const commandsInterval = 5 * 1000
-let running = true
+// let running = true
 
 const clientId = process.env.CLIENT_ID as string
 const clientSecret = process.env.CLIENT_SECRET as string
@@ -34,38 +34,25 @@ async function main() {
 
   const chatClient = new ChatClient(auth, { channels: [channel] })
 
-  let amount: number | string = 1
 
   chatClient.onConnect(async () => {
     console.log("CONNECTED", { channel })
 
-      setInterval(async () => {
-        if (!running) {
-          console.log("STOPPED")
-          return
-        }
-        else { 
-          for (let u = 0; u < coms.length; u++) {
-            const com = coms[u];
-            const message = `!place ${com[0]} ${com[1]} ${com[2]}`
-            await chatClient.say(channel, message).then(
-              () => {
-                console.log("Sent", { message })
-              },
-              (reason) => {
-                console.error("Not sent", { reason })
-              }
-            )
+    coms.forEach(com => {
+      setTimeout(async() => {
+        let message = `!p ${com[0]} ${com[1]} ${com[2]}`
+        await chatClient.say(channel, message).then(
+          () => {
+            console.log("Sent", { message })
+          },
+          (reason) => {
+            console.error("Not sent", { reason })
           }
-          running = false;
-        }
-        // Swap amount to prevent duplicated message
-        if (amount == 1) {
-          amount = "(github.com/narze/autogacha)"
-        } else {
-          amount = 1
-        }
+        )
       }, commandsInterval)
+    })
+    
+
   })
 
   await chatClient.connect()
